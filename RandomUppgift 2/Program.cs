8140aa2904a603ad4f22ce6a4643c6a200f6ead7
Anton <>
2023-03-15 10:41:44 +0100

    Add project files.

---
+++ RandomUppgift 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur många tärningar vill du kasta? ");
+            int AntalTärning = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Hur många sidor ska en tärning ha? ");
+            int AntalSida = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Hur många gånger vill du kasta tärningarna? ");
+            int AntalKast = Convert.ToInt32(Console.ReadLine());
+
+            Random slump = new Random();
+
+            for(int i = 1; i<=AntalKast;i++)
+            {
+                Console.WriteLine("\n Kast " + i + " : ");
+                for(int j = 1; j<=AntalTärning;j++)
+                {
+                    Console.WriteLine("Tärning " + j + " = " + slump.Next(1, AntalSida + 1));
+
+                }
+
+            }
+
+        }
+    }
+}
